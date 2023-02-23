70d94ca02d245acabf751494d7d15e605747c7ea
ryn32233 <rmefford1@gmail.com>
2023-02-23 10:40:52 -0500

    update with fixes

--- app/src/main/java/com/example/classapplication/PokemonAdapter.kt
+++ app/src/main/java/com/example/classapplication/PokemonAdapter.kt
@@ -14,13 +14,13 @@ import com.bumptech.glide.Glide
 import com.example.classapplication.PokemonAdapter.PokemonViewHolder
 import com.example.classapplication.ui.PokemonDetailFragment
 
-class PokemonAdapter(private val pokemon: List<Pokemon>) :
+class PokemonAdapter(private val pokemons: List<Pokemon>) :
     RecyclerView.Adapter<PokemonViewHolder>() {
 
     inner class PokemonViewHolder(
         itemView: View,
-        private val onItemClick: (adapterPosition: Int) -> Unit)
-        : RecyclerView.ViewHolder(itemView) {
+        private val onItemClick: (adapterPosition: Int) -> Unit
+    ) : RecyclerView.ViewHolder(itemView) {
         val pokemonImage: ImageView = itemView.findViewById(R.id.pokemon_image)
         val pokemonName: TextView = itemView.findViewById(R.id.pokemon_name)
         val pokemonType: TextView = itemView.findViewById(R.id.pokemon_type)
@@ -28,54 +28,55 @@ class PokemonAdapter(private val pokemon: List<Pokemon>) :
         val pokemonRegion: TextView = itemView.findViewById(R.id.pokemon_region)
 
         init {
-            itemView.setOnClickListener(
+            itemView.setOnClickListener {
                 onItemClick(adapterPosition)
-           )
+            }
         }
     }
-}
 
-override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PokemonViewHolder {
-    val view = LayoutInflater.from(parent.context)
-        .inflate(R.layout.pokemon_card_view, parent, false)
-    return PokemonViewHolder(view) { position ->
-        val pokemon = pokemon[position]
-        val bundle = bundleOf(
-            "name" to pokemon.name,
-            "type" to pokemon.type,
-            "dexNo" to pokemon.dexNo,
-            "region" to pokemon.region,
-            "image" to pokemon.image
-        )
 
-        val detailFragment: PokemonDetailFragment()
-        detailFragment.arguments = bundle
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PokemonViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.pokemon_card_view, parent, false)
+        return PokemonViewHolder(view) { position ->
+            val pokemon = pokemons[position]
+            val bundle = bundleOf(
+                "name" to pokemon.name,
+                "type" to pokemon.type,
+                "dexNo" to pokemon.dexNo,
+                "region" to pokemon.region,
+                "image" to pokemon.image
+            )
 
-        val activity = view.context as AppCompatActivity
+            val detailFragment = PokemonDetailFragment()
+            detailFragment.arguments = bundle
 
-        activity.supportFragmentManager.commit {
-            setReorderingAllowed(true)
-            replace(R.id.fragment_container_view, detailFragment)
-            addToBackStack(null)
+            val activity = view.context as AppCompatActivity
+
+            activity.supportFragmentManager.commit {
+                setReorderingAllowed(true)
+                replace(R.id.fragment_container_view, detailFragment)
+                addToBackStack(null)
+            }
+
         }
-
     }
+
+    override fun onBindViewHolder(holder: PokemonViewHolder, position: Int) {
+        val pokemon = pokemons[position]
+        //holder.pokemonImage.setImageResource(pokemon.image)
+
+        Glide
+            .with(holder.itemView.context)
+            .load(pokemon.image)
+            .into(holder.pokemonImage)
+
+        holder.pokemonName.text = pokemon.name
+        holder.pokemonType.text = pokemon.type
+        holder.pokemonNo.text = pokemon.dexNo.toString()
+        holder.pokemonRegion.text = pokemon.region
+    }
+
+    override fun getItemCount() = pokemons.size
+
 }
-
-override fun onBindViewHolder(holder: PokemonViewHolder, position: Int) {
-    val pokemon = pokemon[position]
-    //holder.pokemonImage.setImageResource(pokemon.image)
-
-    Glide
-        .with(holder.itemView.context)
-        .load(pokemon.image)
-        .into(holder.pokemonImage)
-
-    holder.pokemonName.text = pokemon.name
-    holder.pokemonType.text = pokemon.type
-    holder.pokemonNo.text = pokemon.dexNo.toString()
-    holder.pokemonRegion.text = pokemon.region
-    }
-
-override fun getItemCount() = pokemon.size
-
